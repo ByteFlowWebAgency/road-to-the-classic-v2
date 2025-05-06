d93cf205ee249121b9ab71322c924fef8d70b17d
Tajohnson245 <tajohnson245@outlook.com>
2025-05-06 19:26:02 -0400

    Update events

--- src/data/events.js
+++ src/data/events.js
@@ -1,7 +1,7 @@
 // Event types
 export const EVENT_TYPES = {
-  ROAD_TO_CLASSIC: 'roadToClassic',
-  HALL_OF_FAME: 'hallOfFame'
+  ROAD_TO_CLASSIC: "roadToClassic",
+  HALL_OF_FAME: "hallOfFame",
 };
 
 export const events = [
@@ -9,210 +9,231 @@ export const events = [
   {
     id: "rtc-1",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Youth Entrepreneur Expo",
-    date: "June 19, 2025",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Youth Entrepreneur Expo",
+    date: "August 16, 2025",
     time: "12:00 PM - 6:00 PM",
     location: "Crenshaw Park",
     host: "Future Promise",
     contact: "Tori Fontes",
     contactEmail: "ToriFontes@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-2",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Cheer & Dance Competition",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Cheer & Dance Competition",
     date: "August 12-14, 2025 (Tues-Thurs)",
     time: "9:00 AM - 3:00 PM (Aug 12-13), 9:00 AM - 12:00 PM (Aug 14)",
-    location: "Edward \"Peel\" Coleman",
+    location: 'Edward "Peel" Coleman',
     host: "Road To The Classic Inc. and We All In",
     contact: "The We All In Team",
     contactEmail: "Weallin2024@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-3",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Youth Football Tournament",
-    date: "August 15-17, 2025 (Fri-Sun)",
-    time: "Friday Registration, Saturday 8:00 AM - 8:00 PM, Sunday 8:00 AM - 2:00 PM",
-    location: "Hall of Fame Football Fields",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Youth Football Tournament",
+    date: "August 16, 2025 (Fri-Sun)",
+    time: "12:00 PM - 6:00 PM",
+    location: "Crenshaw Park",
     host: "L.A.V.A. Athletics",
     contact: "Raymond Trice",
     contactEmail: "raytrice63@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-4",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Spelling Bee",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Spelling Bee",
     date: "August 22, 2025 (Friday)",
-    time: "6:00 PM - 8:00 PM (TBA)",
+    time: "6:00 PM - 8:00 PM",
     location: "Nash Family Center",
-    host: "L.A.V.A. Athletics/Crouse CLC",
+    host: "L.A.V.A. Athletics",
     contact: "Marcus Bentley",
     contactEmail: "mbentley@roadtoclassic.org",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
-    videoLink: "https://www.facebook.com/watch/?v=4243628119197454"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
+    videoLink: "https://www.facebook.com/watch/?v=4243628119197454",
   },
   {
     id: "rtc-5",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU STEAM Fair and Live Inventors Museum",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU STEAM Fair and Live Inventors Museum",
     date: "August 23, 2025",
     time: "10:00 AM - 3:00 PM",
     location: "Hartford STEAM Middle School",
     host: "United Family Community and STEAM Hartford Middle School",
     contact: "Marcus Bentley",
     contactEmail: "mbentley@roadtoclassic.org",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
-  },
-  {
-    id: "rtc-6",
-    type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Youth Entrepreneur Shark Tank",
-    date: "August 23, 2025",
-    time: "10:00 AM - 3:00 PM",
-    location: "Cultural Center (Fry Theatre)",
-    host: "Future Promise",
-    contact: "Tori Fontes",
-    contactEmail: "ToriFontes@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-7",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Hall of Fame HBCU Literacy On The Run",
+    title:
+      "Road To The Classic Black College Hall of Fame HBCU Literacy On The Run",
     date: "August 23, 2025 (Saturday)",
     time: "12:00 PM - 4:00 PM (TBA)",
     location: "HDs Entertainment Center",
     host: "Brooks Rivers Fashion",
     contact: "Jenaye Brooks-Rivers",
     contactEmail: "jenayebrooksrivers@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-8",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame Faith Forward to Fulfillment! HBCU Gospel Concert",
+    title:
+      "Road To The Classic Black College Football Hall of Fame Faith Forward to Fulfillment! HBCU Gospel Concert",
     date: "August 24, 2025 (Sunday)",
     time: "2:00 PM - 5:00 PM (TBA)",
     location: "McKinley Symphony Center (TBA)",
     host: "Walking With a Purpose",
     contact: "Lora Howell",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-9",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Trivia Game Night",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Trivia Game Night",
     date: "August 27, 2025 (Wednesday)",
     time: "6:00 PM - 8:00 PM (TBA)",
     location: "LIVE streamed online",
     host: "Positive Youth Media Network",
     contact: "Marcus Bentley",
     contactEmail: "mbentley@roadtoclassic.org",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-10",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Education Day Podcast Show",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Education Day Podcast Show",
     date: "August 28, 2025 (Thursday)",
     time: "6:00 PM - 8:00 PM (TBA)",
     location: "LIVE streamed online",
     host: "Positive Youth Media Network",
     contact: "Marcus Bentley",
     contactEmail: "mbentley@roadtoclassic.org",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-11",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Road To Royalty: King & Queen Pageant",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Road To Royalty: King & Queen Pageant",
     date: "August 29, 2025 (Friday)",
-    time: "6:00 PM - 9:00 PM (TBA)",
+    time: "6:00 PM - 9:00 PM",
     location: "Nash Family Center",
     host: "Road To The Classic Inc.",
     contact: "Prophet Lisa Smith",
     contactEmail: "theroyalscourt@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "rtc-12",
     type: EVENT_TYPES.ROAD_TO_CLASSIC,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU WE ALL IN Best of Both Worlds Cheer & Dance Competition & Community Parade",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU WE ALL IN Best of Both Worlds Cheer & Dance Competition & Community Parade",
     date: "August 30, 2025 (Saturday)",
     time: "8:00 AM - 3:00 PM",
     location: "Crenshaw Park and Parade Route Sherrick Rd.",
-    host: "Road To the Classic Inc.",
+    host: "Road To the Classic Inc. and WE All in",
     contact: "Marcus Bentley",
     contactEmail: "mbentley@roadtoclassic.org",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
 
   // Hall of Fame Events
   {
     id: "hof-1",
     type: EVENT_TYPES.HALL_OF_FAME,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Youth Entrepreneur Expo",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Youth Entrepreneur Expo",
     date: "Thu, June 19, 2025",
     time: "12:00 PM - 6:00 PM",
     location: "Crenshaw Park",
     host: "Future Promise",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "hof-2",
     type: EVENT_TYPES.HALL_OF_FAME,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Cheer & Dance Competition",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Cheer & Dance Competition",
     date: "August 12-14, 2025 (Tues-Thurs)",
     time: "9:00 AM - 3:00 PM (Aug 12-13), 9:00 AM - 12:00 PM (Aug 14)",
     location: 'Edward "Peel" Coleman',
     host: "Road To The Classic Inc.",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "hof-3",
     type: EVENT_TYPES.HALL_OF_FAME,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Youth Football Tournament",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Youth Football Tournament",
     date: "August 15-17, 2025 (Fri-Sun)",
     time: "Friday Registration, Saturday 8:00 AM - 8:00 PM, Sunday 8:00 AM - 2:00 PM",
     location: "Hall of Fame Football Fields",
     host: "L.A.V.A. Athletics",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "hof-4",
     type: EVENT_TYPES.HALL_OF_FAME,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Spelling Bee",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Spelling Bee",
     date: "Fri, August 22, 2025",
     time: "6:00 PM - 8:00 PM (TBA)",
     location: "Nash Family Center",
     host: "L.A.V.A. Athletics/Crouse CLC",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "hof-5",
     type: EVENT_TYPES.HALL_OF_FAME,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU STEAM Fair and Live Inventors Museum",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU STEAM Fair and Live Inventors Museum",
     date: "Sat, August 23, 2025",
     time: "10:00 AM - 3:00 PM",
     location: "Hartford STEAM Middle School",
     host: "United Family Community and STEAM Hartford Middle School",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
   {
     id: "hof-6",
     type: EVENT_TYPES.HALL_OF_FAME,
-    title: "Road To The Classic Black College Football Hall of Fame HBCU Youth Football Tournament",
+    title:
+      "Road To The Classic Black College Football Hall of Fame HBCU Youth Football Tournament",
     date: "August 16, 2025 (Saturday)",
     time: "12:00 PM - 6:00 PM",
     location: "Crenshaw Park",
     host: "L.A.V.A. Athletics",
     contact: "Raymond Trice",
     contactEmail: "raytrice63@gmail.com",
-    registrationLink: "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform"
-  }
-]; 
-
+    registrationLink:
+      "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
+  },
+];

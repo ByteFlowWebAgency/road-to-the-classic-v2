25576ed9e24fe9dad76e2f71b93e9a56456a9bcb
Tajohnson245 <tajohnson245@outlook.com>
2025-08-13 23:49:40 -0400

    Add HOF events

--- src/data/events.js
+++ src/data/events.js
@@ -133,4 +133,190 @@ export const events = [
     registrationLink:
       "https://docs.google.com/forms/d/e/1FAIpQLSc3YGaQZ0SrHAWXTCy_0xg2-es1jgN4_guudj71Hfxxe0C7Vg/viewform",
   },
+
+  // ---------- Added Hall of Fame Classic events ----------
+  {
+    id: "hof-1",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Reading the Playbook",
+    date: "August 28, 2025 (Thursday)",
+    time: "9:00 AM",
+    location: "Stark & Summit County Schools",
+    host: "Pro Football Hall of Fame & Community Partners (Divine Nine)",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-2",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Classic College Fair",
+    date: "August 28, 2025 (Thursday)",
+    time: "9:00 AM - 12:00 PM",
+    location: "Bulldog Activity Center, 521 Tuscarawas St. W, Canton, OH",
+    host: "Pro Football Hall of Fame",
+    contactEmail: "Education@ProFootballHOF.com",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/bcfhof-classic-college-fair/",
+  },
+  {
+    id: "hof-3",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Black College Football Hall of Fame Community Career Job Fair",
+    date: "August 28, 2025 (Thursday)",
+    time: "4:00 PM - 6:00 PM",
+    location: "Timken Commons Campus, 521 Tuscarawas St. W, Canton, OH",
+    host: "Pro Football Hall of Fame & Ohio Means Jobs",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-4",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Habitat for Humanity Day of Service",
+    date: "August 29, 2025 (Friday)",
+    time: "9:00 AM - 12:00 PM",
+    location: "Habitat for Humanity Campus, 1400 Raff Rd SW, Canton, OH 44710",
+    host: "Habitat for Humanity East Central Ohio",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-5",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Clearview Golf Classic",
+    date: "August 30, 2025 (Saturday)",
+    time: "8:00 AM",
+    location: "Clearview Golf Club (East Canton, OH)",
+    host: "Pro Football Hall of Fame",
+    contact: "Vince Guardado",
+    contactEmail: "Vince.Guardado@ProFootballHOF.com",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-7",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Red S.H.O.E. 5K & 1-Mile Family Walk",
+    date: "August 30, 2025 (Saturday)",
+    time: "8:30 AM",
+    location: "Pro Football Hall of Fame Campus",
+    host: "Stark County Alumnae Chapter of Delta Sigma Theta Sorority, Inc. & Run to You Racing",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-8",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Classic Battle of the Bands",
+    date: "August 30, 2025 (Saturday)",
+    time: "7:00 PM - 9:00 PM",
+    location: "Canton Memorial Field House, 1815 Harrison Ave NW, Canton, OH",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-9",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "2025 Black College Football Hall of Fame Class Plaque Unveiling",
+    date: "August 30, 2025 (Saturday)",
+    time: "4:30 PM",
+    location:
+      "Black College Football Hall of Fame Gallery, Pro Football Hall of Fame",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-10",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Official All White Affair (Community Event)",
+    date: "August 30, 2025 (Saturday)",
+    time: "9:00 PM - 2:30 AM",
+    location: "La Pizzaria, 3656 Dressler Rd NW, Canton, OH",
+    host: "Kappa Tau Chapter of Omega Psi Phi Fraternity, Inc. (Divine Nine)",
+    contact: "Monty Pender (330-754-4376)",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-11",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Cecelia Ponder Time of Inspiration (Community Event)",
+    date: "August 31, 2025 (Sunday)",
+    time: "9:00 AM - 10:30 AM",
+    location: "Gold Jacket Lounge, Pro Football Hall of Fame",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-12",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "HBCU Family Block Party presented by CSE Federal Credit Union",
+    date: "August 31, 2025 (Sunday)",
+    time: "11:00 AM - 3:30 PM",
+    location: "Pro Football Hall of Fame Campus",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-13",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "The Classic Tailgate presented by Sugardale",
+    date: "August 31, 2025 (Sunday)",
+    time: "11:00 AM - 3:30 PM",
+    location: "Pro Football Hall of Fame Campus",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-14",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Faith, Family & Football Gospel Concert (featuring Tamela Mann)",
+    date: "August 31, 2025 (Sunday)",
+    time: "1:30 PM",
+    location: "Pro Football Hall of Fame Campus",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-15",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "Minority Business Reception",
+    date: "August 31, 2025 (Sunday)",
+    time: "11:30 AM - 1:30 PM",
+    location: "Pro Football Hall of Fame",
+    host: "Canton Regional Chamber of Commerce, SEDB, SCMBA & Stark County Moving Forward",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
+  {
+    id: "hof-16",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title:
+      "2025 Clash at the Classic Game: Miles College vs. Virginia Union University",
+    date: "August 31, 2025 (Sunday)",
+    time: "4:00 PM",
+    location: "Tom Benson Hall of Fame Stadium",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+    videoLink:
+      "https://vuusports.com/news/2025/2/7/vuu-releases-2025-football-schedule.aspx",
+  },
+  {
+    id: "hof-17",
+    type: EVENT_TYPES.HALL_OF_FAME,
+    title: "The Fifth Quarter (Postgame Band Battle)",
+    date: "August 31, 2025 (Sunday)",
+    time: "Immediately following the game",
+    location: "Tom Benson Hall of Fame Stadium",
+    host: "Pro Football Hall of Fame",
+    registrationLink:
+      "https://www.profootballhof.com/black-college-football-hall-of-fame-classic/schedule-of-events/",
+  },
 ];
